5cf896528477095d072d2eb78e3b4e31e79b9410
shubinfilms <shubinfilms@mail.ru>
2025-09-27 18:53:31 +0500

    Implement multi-engine Prompt-Master generator

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,11 @@
 """Public handler shortcuts."""
 
 from .faq_handler import configure_faq, faq_callback, faq_command
-from .prompt_master_handler import prompt_master_callback, prompt_master_open
+from .prompt_master_handler import (
+    prompt_master_callback,
+    prompt_master_open,
+    prompt_master_process,
+)
 
 __all__ = [
     "configure_faq",
@@ -9,4 +13,5 @@ __all__ = [
     "faq_command",
     "prompt_master_callback",
     "prompt_master_open",
+    "prompt_master_process",
 ]
